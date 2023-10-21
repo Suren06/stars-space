9b484260ec925aa3f4cda95352bd7210e6caba0a
Suren06 <suren.boyilla@gmail.com>
2023-10-21 19:31:13 +0800

    Final

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,86 +1,74 @@
 import { FaSearch } from "react-icons/fa";
-import { BiHome } from "react-icons/bi";
-import { TbArrowRoundaboutRight } from "react-icons/tb";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const Header = () => {
+export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  const location = useLocation();
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
 
-  function pathMatchRoute(route) {
-    if (route === location.pathname) {
-      console.log("location.pathname:", location.pathname);
-      console.log("route:", route);
-      return;
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
     }
-  }
-
+  }, [location.search]);
   return (
     <header className="shadow-md bg-slate-200">
       <div className="flex items-center justify-between max-w-6xl p-3 mx-auto">
         <Link to="/">
-          <h1 className="flex flex-wrap gap-1 text-sm font-bold sm:text-xl">
-            <span className="text-slate-500">Suren </span>
-            <span className="text-slate-700"> Estate</span>
+          <h1 className="flex flex-wrap text-sm font-bold sm:text-xl">
+            <span className="text-slate-600">Stars Space</span>
           </h1>
         </Link>
-        <form className="flex items-center p-3 mt-3 rounded-lg bg-slate-100">
+        <form
+          onSubmit={handleSubmit}
+          className="flex items-center p-3 rounded-lg bg-slate-100"
+        >
           <input
             type="text"
-            placeholder="Search.."
+            placeholder="Search..."
             className="w-24 bg-transparent focus:outline-none sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
-
-        <ul className="flex gap-4 ">
-          <Link to="/" className="">
-            <li
-              className={`flex items-center gap-[3px] px-2 hover:bg-gray-300 rounded-lg  cursor-pointer py-2 font-semibold text-slate-600 border-b-[3px] 
-              border-b-transparent hover:text-blue-700 ${
-                pathMatchRoute("/") && "text-blue-700 border-b-blue-700"
-              }`}
-            >
-              <BiHome size="16px" />
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
               Home
             </li>
           </Link>
-
-          <Link to="/about" className="inline-flex">
-            <li
-              className={`flex items-center gap-[3px] px-2 hover:bg-gray-300 rounded-lg cursor-pointer py-2 font-semibold text-slate-600 border-b-[3px]
-               border-b-transparent hover:text-blue-700 ${
-                 pathMatchRoute("/about") && "text-blue-700 border-b-blue-700"
-               }`}
-            >
-              <TbArrowRoundaboutRight size="16px" />
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
               About
             </li>
           </Link>
-
           <Link to="/profile">
             {currentUser ? (
               <img
-                className="object-cover w-10 h-10 rounded-full"
-                src={currentUser?.avatar}
+                className="object-cover rounded-full h-7 w-7"
+                src={currentUser.avatar}
                 alt="profile"
               />
             ) : (
-              <li
-                className={`flex items-center gap-[3px] px-2 hover:bg-gray-300 rounded-lg cursor-pointer py-2 font-semibold text-slate-600 border-b-[3px]
-               border-b-transparent hover:text-blue-700 ${
-                 pathMatchRoute("/sign-in") && "text-blue-700 border-b-blue-700"
-               }`}
-              >
-                Sign in
-              </li>
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
             )}
           </Link>
         </ul>
       </div>
     </header>
   );
-};
-
-export default Header;
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,24 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primaryColor: "#0067FF",
-        yellowColor: "#FEB60D",
-        purpleColor: "#9771FE",
-        irisBlueColor: "#01B5C5",
-        headingColor: "#181A1E",
-        textColor: "#4E545F",
-      },
-      boxShadow: {
-        panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require("@tailwindcss/line-clamp"),
+    // ...
+  ],
+};
